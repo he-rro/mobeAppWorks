7abec2aa9c68887c03ed32f1103d4ccb58572b95
hero <hero.007372@gmail.com>
2025-08-13 00:08:56 -0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF", 
+        padding: 20,
+        borderColor: "#000",
+        borderWidth: 1,
+        borderRadius: 10,
+        shadowColor: "#000",
+      }}
+    >
+      <Text>hellow react native this is my firs project by by.</Text>
+    </View>
+  );
+}
